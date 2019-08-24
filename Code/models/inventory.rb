d759f4dfb674aecaaad77dd72276cadaec87e937
hayden <haydenwb@gmail.com>
2019-08-24 15:36:54 +0100

    added reduce methods to inventory class - needs refactoring

--- Code/models/inventory.rb
+++ Code/models/inventory.rb
@@ -27,8 +27,30 @@ class Inventory
     SqlRunner.run(sql)
   end
 
+  def return_quantity
+    sql = "SELECT inventory.quantity FROM inventory
+    WHERE id = $1"
+    values = [@id]
+    quantity = SqlRunner.run(sql, values)
+    quantity.first()['quantity'].to_i
+  end
+
+  def reduce_quantity
+    new_quantity = return_quantity
+    new_quantity -= 1
+  end
+
+  def reduce_quantity_in_database
+    new_quantity = reduce_quantity
+    sql = "UPDATE inventory
+    SET quantity = $1
+    WHERE id = $2"
+    values = [new_quantity, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def reduce_quantity_by_one()
-    quantity =- 1
+    quantity = 1
     sql = "UPDATE inventory
     SET quantity = $1
     WHERE id = $2"
