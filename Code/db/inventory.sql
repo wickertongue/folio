fc1daddb6fa45ff5db3ac8afa4c3b0f81f9dc5d0
hayden <haydenwb@gmail.com>
2019-08-24 10:50:06 +0100

    database created, tables set

---
+++ Code/db/inventory.sql
@@ -0,0 +1,26 @@
+DROP TABLE authors;
+DROP TABLE books;
+DROP TABLE inventory;
+
+CREATE TABLE authors (
+  id SERIAL8 primary key,
+  forename VARCHAR(255) not null,
+  surname VARCHAR(255) not null
+);
+
+CREATE TABLE books (
+  id SERIAL8 primary key,
+  title VARCHAR(255) not null,
+  publisher VARCHAR(255) not null,
+  genre VARCHAR(255),
+  description VARCHAR(255) not null,
+  cost_to_sell NUMERIC not null,
+  cost_to_purchase NUMERIC not null
+);
+
+CREATE TABLE inventory (
+  id SERIAL8 primary key,
+  author_id INT8 references authors(id),
+  book_id INT8 references books(id),
+  quantity INT8
+);
