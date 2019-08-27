e34312bb8ab78027c9b5b2364dc56c6908890404
hayden <haydenwb@gmail.com>
2019-08-27 22:32:31 +0100

    finally created all form fills, phew

--- Code/controllers/inventory_controller.rb
+++ Code/controllers/inventory_controller.rb
@@ -16,12 +16,31 @@ get '/inventory/?' do
   erb(:index)
 end
 
+get '/inventory/new_item' do
+  @inventory = Inventory.all()
+  @books = Book.all()
+  @authors = Author.all()
+  erb(:"inventory/new")
+end
+
+post '/inventory/submitted_item' do
+  Inventory.new(params).save
+  redirect to '/inventory'
+end
+
 get '/inventory/:id' do
   @inventory = Inventory.find(params['id'].to_i)
   @book = Book.find(@inventory.book_id)
   erb(:show)
 end
 
-get '/inventory/new_author' do
-  erb(:"author/new")
-end
+
+# post '/inventory/submitted_item' do
+#   new_item = Inventory.new(params)
+#   new_item.save
+#   junction_hash = {"book_id" => params["book_id"]}
+#   Inventory.new(junction_hash).save
+#   binding.pry
+#   nil
+#   redirect to '/inventory'
+# end
