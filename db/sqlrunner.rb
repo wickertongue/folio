3577b90b35cfa57a817170284a01bdefa514416f
hayden <haydenwb@gmail.com>
2019-10-07 15:19:43 +0100

    amended database for heroku

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -7,7 +7,10 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'inventory', host: 'localhost' })
+      # db = PG.connect({ dbname: 'inventory', host: 'localhost' })
+
+      db = PG.connect({ dbname: 'd394nl18js8esc', host: 'ec2-174-129-43-40.compute-1.amazonaws.com', port: 5432, user: 'eqzgfecnzkkejo', password: '416324330abcb2c7f87f22e95e936d213eb68e37efd98abdc6e30d5760cd138e' })
+
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
